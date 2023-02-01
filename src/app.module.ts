0891322a50d45b6483866823b3edd91e6976a086
ihyun <okyee8335@naver.com>
2023-02-01 16:05:12 +0900

    010223 16:00 ihyun prac)NestJS-@Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+//module 소스 - 모듈을 정의합니다.(controller와 service 정의)
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
