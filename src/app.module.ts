40eb3fc07a50aa6e60949e6eb7e5c9a2c0df21ef
ihyun <okyee8335@naver.com>
2023-02-14 19:15:33 +0900

    140223 19:00 ihyun prac)NestJS-middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestMiddleware, RequestMethod} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import {AuthMiddleware} from "./middleware/auth.middleware";
+
 //module 소스 - 모듈을 정의합니다.(controller와 service 정의)
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestMiddleware {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+        .apply(AuthMiddleware)
+        .forRoutes({path: 'users', method: RequestMethod.GET});
+  }
+  use(req: any, res: any, next: (error?: any) => void): any {
+  }
+}
