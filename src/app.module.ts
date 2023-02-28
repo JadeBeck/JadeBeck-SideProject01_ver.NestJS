619e708eac0c314e3ca51ccfe99ec305e5a2ad88
ihyun <okyee8335@naver.com>
2023-02-28 14:35:23 +0900

    280223 14:00 ihyun prac)NestJS-TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,36 @@
-import { MiddlewareConsumer, Module, NestMiddleware, RequestMethod} from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import {MiddlewareConsumer, Module, NestMiddleware, RequestMethod} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import { User } from './users/entity/users.entity';
 import {AuthMiddleware} from "./middleware/auth.middleware";
 
 //module 소스 - 모듈을 정의합니다.(controller와 service 정의)
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 13306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            entities: [User],
+            synchronize: true,  //synchronize: true는 운영에서는 사용하지 마세요.
+        }),
+        UsersModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 
 export class AppModule implements NestMiddleware {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-        .apply(AuthMiddleware)
-        .forRoutes({path: 'users', method: RequestMethod.GET});
-  }
-  use(req: any, res: any, next: (error?: any) => void): any {
-  }
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware)
+            .forRoutes({path: 'users', method: RequestMethod.GET});
+    }
+
+    use(req: any, res: any, next: (error?: any) => void): any {
+    }
 }
