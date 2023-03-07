d1efc7ed0dddc190301dc1774701af944b0708f4
ihyun <okyee8335@naver.com>
2023-03-07 15:45:33 +0900

    070323 16:00 ihyun prac)signup..ing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Body, Post, Req} from '@nestjs/common';
+import {Request} from 'express';
+import {AuthService} from "./auth.service";
+import {UserDTO} from './dto/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {
+    }
+
+    @Post('/register')
+    async registerAccount(@Req() req: Request, @Body() UserDTO: UserDTO): Promise<any> {
+        return await this.authService.registerUser(UserDTO);
+    }
+}
