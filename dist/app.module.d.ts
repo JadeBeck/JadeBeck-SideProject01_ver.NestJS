cc0b145f9cc93c8404ed01e6de8feb4142806b9b
ihyun <okyee8335@naver.com>
2023-04-09 18:55:12 +0900

    remove gitignore files

--- dist/app.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { MiddlewareConsumer, NestMiddleware } from '@nestjs/common';
-export declare class AppModule implements NestMiddleware {
-    configure(consumer: MiddlewareConsumer): void;
-    use(req: any, res: any, next: (error?: any) => void): any;
-}
